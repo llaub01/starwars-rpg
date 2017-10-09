6e63bce7c032c5b45b2e0f77f7ce741049011302
Lonnie Laub <llaub01@gmail.com>
2017-10-09 18:35:06 -0500

    commit again

---
+++ assets/scripts/game.js
@@ -0,0 +1,26 @@
+// global variables
+// character arrays - name, hitpoints, attack en, attack pl, pl attack increment
+var chewbacca = ["Chewbacca", 150, 15, 20, 7];
+var yoda = ["Yoda", 100, 20, 10, 10];
+var ewoks = ["Ewoks", 125, 15, 18, 8];
+var hansolo = ["Han Solo", 120, 18, 15, 9];
+
+$("#player").on("click", function() {
+	for
+}
+
+var startGame = function() {
+
+}
+
+var pickPlayer = function() {
+	var
+}
+
+var attack = function() {
+
+}
+
+var restart = function() {
+	//set object and variables back to empty/0
+}
